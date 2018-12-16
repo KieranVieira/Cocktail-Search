2d782ff2b1f4ed5d8e1bb688cebcb556568889a5
KieranVieira <kieranvieira@live.com>
2018-12-16 14:45:20 -0500

    Fixed styling error

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -108,11 +108,11 @@ class RecipeReviewCard extends React.Component {
         </CardActions>
         <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
           <CardContent>
-            <Typography paragraph className={headerStyle}>Description:</Typography>
+            <Typography paragraph className={classes.headerStyle}>Description:</Typography>
             <Typography paragraph>
               {description}
             </Typography>
-            <Typography paragraph className={headerStyle}>Ingredients:</Typography>
+            <Typography paragraph className={classes.headerStyle}>Ingredients:</Typography>
             <div className={classes.ingredientFlex}><Typography paragraph>{ingredient1 ? ingredient1 : ''}</Typography> <Typography> {measurement1 ? measurement1 : ''}</Typography></div>
             <div className={classes.ingredientFlex}><Typography paragraph>{ingredient2 ? ingredient2 : ''}</Typography> <Typography>{measurement2 ? measurement2 : ''}</Typography></div>
             <div className={classes.ingredientFlex}><Typography paragraph>{ingredient3 ? ingredient3 : ''}</Typography> <Typography> {measurement3 ? measurement3 : ''}</Typography></div>
