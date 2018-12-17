50fe44a52bb540e27208ab5f005b87c1e56165f6
john2796 <benedictmiranda27@yahoo.com>
2018-12-17 10:42:43 -0800

    updated categories tab

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -108,7 +108,7 @@ class NavBar extends Component {
     e.preventDefault();
     fetch(
       `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${
-        this.state.searchTerm
+      this.state.searchTerm
       }`
     )
       .then(res => res.json())
@@ -119,16 +119,16 @@ class NavBar extends Component {
       .then(res => res.json())
       .then(data => this.setState({ data }));
   };
+
   getCats = e => {
-    let drinks = {};
     fetch(
-      `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${
-        e.target.value
+      `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${
+      e.target.value
       }`
     )
       .then(res => res.json())
       .then(data => this.setState({ data }));
-      // .then(data => this.getCategoryObj(data));
+    // .then(data => this.getCategoryObj(data));
   };
   // getCategoryObj = (data) => {
   //   let realDrinks = {
@@ -141,7 +141,7 @@ class NavBar extends Component {
   //   });
   //   console.log(realDrinks)
   // }
-  getAlcoholic = () => {};
+  getAlcoholic = () => { };
   render() {
     const { classes } = this.props;
     const { data, categories } = this.state;
@@ -216,49 +216,49 @@ class NavBar extends Component {
               Go home, you're drunk. We can't serve you.
             </h1>
           ) : (
-            data.drinks.map(drink => (
-              <Grid key={drink.idDrink} item xs={12} sm={6} md={3}>
-                <Card
-                  key={drink.idDrink}
-                  title={drink.strDrink}
-                  category={drink.strCategory}
-                  description={drink.strInstructions}
-                  img={drink.strDrinkThumb}
-                  date={drink.dateModified}
-                  ingredient1={drink.strIngredient1}
-                  ingredient2={drink.strIngredient2}
-                  ingredient3={drink.strIngredient3}
-                  ingredient4={drink.strIngredient4}
-                  ingredient5={drink.strIngredient5}
-                  ingredient6={drink.strIngredient6}
-                  ingredient7={drink.strIngredient7}
-                  ingredient8={drink.strIngredient8}
-                  ingredient9={drink.strIngredient9}
-                  ingredient10={drink.strIngredient10}
-                  ingredient11={drink.strIngredient11}
-                  ingredient12={drink.strIngredient12}
-                  ingredient13={drink.strIngredient13}
-                  ingredient14={drink.strIngredient14}
-                  ingredient15={drink.strIngredient15}
-                  measurement1={drink.strMeasure1}
-                  measurement2={drink.strMeasure2}
-                  measurement3={drink.strMeasure3}
-                  measurement4={drink.strMeasure4}
-                  measurement5={drink.strMeasure5}
-                  measurement6={drink.strMeasure6}
-                  measurement7={drink.strMeasure7}
-                  measurement8={drink.strMeasure8}
-                  measurement9={drink.strMeasure9}
-                  measurement10={drink.strMeasure10}
-                  measurement11={drink.strMeasure11}
-                  measurement12={drink.strMeasure12}
-                  measurement13={drink.strMeasure13}
-                  measurement14={drink.strMeasure14}
-                  measurement15={drink.strMeasure15}
-                />
-              </Grid>
-            ))
-          )}
+              data.drinks.map(drink => (
+                <Grid key={drink.idDrink} item xs={12} sm={6} md={3}>
+                  <Card
+                    key={drink.idDrink}
+                    title={drink.strDrink}
+                    category={drink.strCategory}
+                    description={drink.strInstructions}
+                    img={drink.strDrinkThumb}
+                    date={drink.dateModified}
+                    ingredient1={drink.strIngredient1}
+                    ingredient2={drink.strIngredient2}
+                    ingredient3={drink.strIngredient3}
+                    ingredient4={drink.strIngredient4}
+                    ingredient5={drink.strIngredient5}
+                    ingredient6={drink.strIngredient6}
+                    ingredient7={drink.strIngredient7}
+                    ingredient8={drink.strIngredient8}
+                    ingredient9={drink.strIngredient9}
+                    ingredient10={drink.strIngredient10}
+                    ingredient11={drink.strIngredient11}
+                    ingredient12={drink.strIngredient12}
+                    ingredient13={drink.strIngredient13}
+                    ingredient14={drink.strIngredient14}
+                    ingredient15={drink.strIngredient15}
+                    measurement1={drink.strMeasure1}
+                    measurement2={drink.strMeasure2}
+                    measurement3={drink.strMeasure3}
+                    measurement4={drink.strMeasure4}
+                    measurement5={drink.strMeasure5}
+                    measurement6={drink.strMeasure6}
+                    measurement7={drink.strMeasure7}
+                    measurement8={drink.strMeasure8}
+                    measurement9={drink.strMeasure9}
+                    measurement10={drink.strMeasure10}
+                    measurement11={drink.strMeasure11}
+                    measurement12={drink.strMeasure12}
+                    measurement13={drink.strMeasure13}
+                    measurement14={drink.strMeasure14}
+                    measurement15={drink.strMeasure15}
+                  />
+                </Grid>
+              ))
+            )}
         </Grid>
       </div>
     );
